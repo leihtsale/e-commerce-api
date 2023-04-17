85059b204ed24a95a59e489ce2280ee961cfc741
elasthiel <elasthiel@gmail.com>
2023-04-18 07:12:37 +0800

    Modify serializers and views to handle categories

--- backend/src/products/serializers.py
+++ backend/src/products/serializers.py
@@ -1,14 +1,17 @@
-from core.models import Product
+from categories.serializers import CategorySerializer
+from core.models import Category, Product
 from rest_framework import serializers
 
 
 class ProductSerializer(serializers.ModelSerializer):
 
+    categories = CategorySerializer(many=True, required=False)
+
     class Meta:
         model = Product
         fields = [
-            'name', 'price', 'inventory',
-            'description', 'rating', 'total_sold',
+            'name', 'price', 'inventory', 'description',
+            'rating', 'total_sold', 'categories',
             'created_at', 'updated_at']
         extra_kwargs = {
             'rating': {'read_only': True},
@@ -16,6 +19,63 @@ class ProductSerializer(serializers.ModelSerializer):
             'created_at': {'read_only': True},
             'updated_at': {'read_only': True}
         }
+
+    def _get_categories(self, categories):
+        """
+        Helper function to get the categories by name
+        :return: Array of Category
+        """
+        category_list = []
+        for category in categories:
+            try:
+                existing_category = Category.objects.get(
+                    name=category['name'].lower()
+                )
+                category_list.append(existing_category)
+            except Category.DoesNotExist:
+                raise serializers.ValidationError(
+                    f"Category not found: {category['name']}"
+                )
+        return category_list
+
+    def create(self, validated_data):
+        """
+        Modify the create, to remove the categories in validate_data
+        and get all the categories by name with _get_categories
+        helper function. And if the categories exists,
+        assign it to the product
+        """
+        categories = validated_data.pop('categories', [])
+        existing_categories = self._get_categories(categories)
+        product = Product.objects.create(**validated_data)
+
+        for category in existing_categories:
+            product.categories.add(category)
+
+        return product
+
+    def update(self, instance, validated_data):
+        """
+        Modify the update, to remove the categories in validate_data
+        and get all the categories by name with _get_categories
+        helper function. And if the categories exists,
+        assign it to the product
+        """
+        categories = validated_data.pop('categories', [])
+
+        if categories is not None:
+            instance.categories.clear()
+            existing_categories = self._get_categories(categories)
+
+            for category in existing_categories:
+                instance.categories.add(category)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+
+        return instance
 
     def to_internal_value(self, data):
         """
--- backend/src/products/views.py
+++ backend/src/products/views.py
@@ -9,7 +9,14 @@ class ProductViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
+    def get_permissions(self):
+        if (self.action == 'list' or self.action == 'retrieve'):
+            return [permissions.AllowAny()]
+        return super().get_permissions()
+
     def get_queryset(self):
+        if (self.action == 'list' or self.action == 'retrieve'):
+            return Product.objects.all()
         return Product.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
