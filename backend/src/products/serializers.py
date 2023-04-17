7ed7bef342e43f06b08bae96bd0895e4dcfeec2c
elasthiel <elasthiel@gmail.com>
2023-04-17 13:14:10 +0800

    Add comment and sort imports

--- backend/src/products/serializers.py
+++ backend/src/products/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework import serializers
 from core.models import Product
+from rest_framework import serializers
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -18,6 +18,12 @@ class ProductSerializer(serializers.ModelSerializer):
         }
 
     def to_internal_value(self, data):
+        """
+        This method raises an error if there is an attempt to update
+        a read_only field, instead of just ignoring it.
+
+        :raises: ValidationError
+        """
         for field_name, field in self.fields.items():
             if field.read_only and field_name in data:
                 raise serializers.ValidationError({
