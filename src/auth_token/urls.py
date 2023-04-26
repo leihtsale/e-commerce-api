f7ad2385006221d91124b6b37866febc0f98207b
elasthiel <elasthiel@gmail.com>
2023-04-26 18:27:35 +0800

    Remove the old authentication app

--- src/auth_token/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'token'
-
-urlpatterns = [
-    path('create/', views.CreateTokenView.as_view(), name='create'),
-]
--- src/auth_token/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.authtoken.views import ObtainAuthToken
-from . import serializers
-
-
-class CreateTokenView(ObtainAuthToken):
-    """
-    Make the ObtainAuthToken to use the custom serializer
-    """
-    serializer_class = serializers.TokenSerializer
