19135b2e72069bda27b22114b5d9f106cf602e41
elasthiel <elasthiel@gmail.com>
2023-04-26 18:21:55 +0800

    Add view to set the tokens as http only cookies

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.settings import api_settings
 
-# Create your views here.
+class CookieTokenObtainView(TokenObtainPairView):
+    def finalize_response(self, request, response, *args, **kwargs):
+        if response.status_code == 200:
+            access_token = response.data.pop('access')
+            refresh_token = response.data.pop('refresh')
+            response.set_cookie(
+                'refresh_token', refresh_token,
+                httponly=True,
+                max_age=api_settings.REFRESH_TOKEN_LIFETIME.total_seconds(),
+            )
+            response.set_cookie(
+                'access_token', access_token,
+                httponly=True,
+                max_age=api_settings.ACCESS_TOKEN_LIFETIME.total_seconds(),
+            )
+        return super().finalize_response(request, response, *args, **kwargs)
