befa03d912befd3296421aaf6f8e2b763269ef87
elasthiel <elasthiel@gmail.com>
2023-04-28 10:45:38 +0800

    Add the modified urls of products

--- src/api_routes/urls.py
+++ src/api_routes/urls.py
@@ -1,12 +1,11 @@
-from carts.urls import router as carts_router
-from categories.urls import router as categories_router
 from django.urls import include, path
-from orders.urls import router as orders_router
-from products.urls import router as product_router
 from rest_framework.routers import SimpleRouter
 
+from carts.urls import router as carts_router
+from categories.urls import router as categories_router
+from orders.urls import router as orders_router
+
 router = SimpleRouter()
-router.registry.extend(product_router.registry)
 router.registry.extend(carts_router.registry)
 router.registry.extend(categories_router.registry)
 router.registry.extend(orders_router.registry)
@@ -14,5 +13,6 @@ router.registry.extend(orders_router.registry)
 urlpatterns = [
     path('user/', include('user.urls')),
     path('token/', include('authentication.urls')),
+    path('products/', include('products.urls')),
     path('', include((router.urls, 'api')))
 ]
