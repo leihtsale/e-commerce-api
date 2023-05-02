5c9e98647188a1d3c67fdad3fad32d42202e62ac
elasthiel <elasthiel@gmail.com>
2023-05-02 17:23:46 +0800

    Register all the models

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from core.models import (Product, Category,
+                         Cart, OrderItem, Order)
+
 
 admin.site.register(get_user_model())
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Cart)
+admin.site.register(OrderItem)
+admin.site.register(Order)
