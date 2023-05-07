55ead7d61a8c7dfedbb50f5dd7b0c7a50f6878ec
elasthiel <elasthiel@gmail.com>
2023-05-07 17:01:37 +0800

    Add separate endpoint for direct checkout

--- src/payments/urls.py
+++ src/payments/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from payments.views import CreateStripeCheckoutSession, StripeWebHook
+from payments.views import (CartCheckoutSession, DirectCheckoutSession,
+                            StripeWebHook)
 
 app_name = 'payments'
 
 urlpatterns = [
-    path('checkout_session/', CreateStripeCheckoutSession.as_view(),
-         name='checkout_session'),
     path('stripe_webhook', StripeWebHook.as_view(), name='stripe_webhook'),
+    path('direct_checkout/', DirectCheckoutSession.as_view(), name='direct_checkout'),
+    path('cart_checkout/', CartCheckoutSession.as_view(),
+         name='cart_checkout'),
 ]
--- src/payments/views.py
+++ src/payments/views.py
@@ -1,5 +1,7 @@
 import stripe
 from django.conf import settings
+from django.db import transaction
+from django.db.utils import IntegrityError
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status, views
@@ -7,13 +9,16 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from core.models import Order
+from core.models import Order, OrderItem, Product
 from orders.serializers import OrderSerializer
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 class StripeWebHook(views.APIView):
+    """
+    Stripe webhook for success checkout
+    """
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
         return super(StripeWebHook, self).dispatch(*args, **kwargs)
@@ -35,34 +40,75 @@ class StripeWebHook(views.APIView):
             return Response({'error': str(e)},
                             status=status.HTTP_400_BAD_REQUEST)
 
-        print(event['type'])
         if event['type'] == 'checkout.session.completed':
             session = event['data']['object']
-            handle_successful_payment(session)
+            status_code, error_msg = handle_successful_payment(session)
+
+            if status_code != status.HTTP_200_OK:
+                return Response({'error': error_msg}, status=status_code)
 
         return Response({}, status=status.HTTP_200_OK)
 
 
-def handle_successful_payment(session):
-    order_id = session['metadata']['order_id']
+class DirectCheckoutSession(views.APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
-    try:
-        order = Order.objects.get(id=order_id)
-    except Order.DoesNotExist:
-        print(f"Order {order_id} not found.")
-        return
+    def post(self, request, *args, **kwargs):
+        shipping_info = request.data.get('shipping_info', {})
+        user = request.user
+        product_id = request.data.get('product_id')
+        quantity = request.data.get('quantity')
 
-    serializer = OrderSerializer(
-        order, data={'status': Order.PAID}, partial=True)
+        try:
+            product = Product.objects.get(pk=product_id)
+        except Product.DoesNotExist:
+            return Response({'error': 'Product does not exists.'}, status=status.HTTP_400_BAD_REQUEST)
 
-    if serializer.is_valid():
-        print(f"Order {order_id} is valid, updating status to PAID.")
-        serializer.save()
-    else:
-        print(f"Error updating order {order_id}: {serializer.errors}")
+        if quantity <= product.inventory and quantity > 0:
+
+            with transaction.atomic():
+                try:
+                    order = Order.objects.create(
+                        user=user, shipping_info=shipping_info, status=Order.PENDING)
+
+                    order_item = OrderItem.objects.create(
+                        order=order, product=product,
+                        unit_price=product.price, quantity=quantity)
+
+                    product.inventory -= quantity
+                    product.total_sold += quantity
+                    product.save()
+
+                except IntegrityError:
+                    return Response(
+                        {'error': 'Unable to process your order.'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+        item = [{
+                'price_data': {
+                    'currency': 'PHP',
+                    'product_data': {
+                        'name': order_item.product.name,
+                    },
+                    'unit_amount': int(order_item.unit_price * 100),
+                },
+                'quantity': order_item.quantity,
+                }]
+
+        status_code, data = create_stripe_checkout(item, order.id)
+
+        if status_code != status.HTTP_200_OK:
+            return Response({'error': data}, status=status_code)
+
+        return Response({'id': data}, status=status.HTTP_200_OK)
 
 
-class CreateStripeCheckoutSession(views.APIView):
+class CartCheckoutSession(views.APIView):
+    """
+    View for creating a Stripe session
+    success: returns a session id
+    """
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
@@ -85,31 +131,63 @@ class CreateStripeCheckoutSession(views.APIView):
         pending_order = order_serializer.save(user=user)
 
         order_items = []
-
-        try:
-            for order_item in pending_order.order_items.all():
-                order_items.append({
-                    'price_data': {
-                        'currency': 'PHP',
-                        'product_data': {
-                            'name': order_item.product.name,
-                        },
-                        'unit_amount': int(order_item.unit_price * 100),
+        for order_item in pending_order.order_items.all():
+            order_items.append({
+                'price_data': {
+                    'currency': 'PHP',
+                    'product_data': {
+                        'name': order_item.product.name,
                     },
-                    'quantity': order_item.quantity,
-                })
-
-            checkout_session = stripe.checkout.Session.create(
-                line_items=order_items,
-                mode='payment',
-                metadata={
-                    'order_id': pending_order.id
+                    'unit_amount': int(order_item.unit_price * 100),
                 },
-                success_url=settings.PAYMENT_SUCCESS_URL
-            )
-        except Exception as e:
-            print(e)
-            return Response({'error': 'Something went wrong.'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                'quantity': order_item.quantity,
+            })
 
-        return Response({'id': checkout_session.id}, status=status.HTTP_200_OK)
+        status_code, data = create_stripe_checkout(
+            order_items, pending_order.id)
+
+        if status_code != status.HTTP_200_OK:
+            return Response({'error': data}, status=status_code)
+
+        return Response({'id': data}, status=status.HTTP_200_OK)
+
+
+"""
+Helper functions
+"""
+
+
+def create_stripe_checkout(line_items, order_id):
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=line_items,
+            mode='payment',
+            metadata={
+                'order_id': order_id
+            },
+            success_url=settings.PAYMENT_SUCCESS_URL
+        )
+    except Exception as e:
+        return status.HTTP_500_INTERNAL_SERVER_ERROR, f'Something went wrong.'
+
+    Order.objects.filter(id=order_id).update(
+        stripe_checkout_session_id=checkout_session.id)
+    return status.HTTP_200_OK, checkout_session.id
+
+
+def handle_successful_payment(session):
+    order_id = session['metadata']['order_id']
+
+    try:
+        order = Order.objects.get(id=order_id)
+    except Order.DoesNotExist:
+        return status.HTTP_404_NOT_FOUND, f'Order with ID {order_id} does not exist'
+
+    serializer = OrderSerializer(
+        order, data={'status': Order.PAID}, partial=True)
+
+    if serializer.is_valid():
+        serializer.save()
+        return status.HTTP_200_OK, ''
+    else:
+        return status.HTTP_400_BAD_REQUEST, f'Failed to update Order with ID {order_id}.'
